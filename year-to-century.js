38d0e34f7ce43fb9eee7fd8110451e05363f586f
JakesProjects1 <jacob@kagon.net>
2021-02-18 18:27:30 -0500

    done

---
+++ year-to-century.js
@@ -0,0 +1,7 @@
+//Takes in an argument of a year and you have to return what century that year is in. Read the question!! Not that hard bro
+
+function century(year) {
+    // Finish this :)
+    return Math.floor((year + 99) / 100)
+    
+    }
