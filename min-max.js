132f41f23d2b9e8590130c78aecd9c886423a494
JakesProjects1 <jacob@kagon.net>
2021-03-08 18:46:13 -0500

    codewars

---
+++ min-max.js
@@ -0,0 +1,29 @@
+var min = function(list){
+
+    return Math.min(...list)
+   // let min;
+     
+   //   for(let i = 0; i < list.length; i++) {
+   //     min = list[i]
+   //     if(min > list[i + 1]) {
+   //       min = list[i + 1]
+   //     }
+   //   }
+   //   return min
+   
+   }
+   
+   var max = function(list){
+       
+   // let max;
+     
+   //   for(let i = 0; i < list.length; i++) {
+   //     max = list[i]
+   //     if (max < list[i + 1]) {
+   //       max = list[i + 1]
+   //     }
+   //   }
+   //   return max
+     
+     return Math.max(...list)
+   }
