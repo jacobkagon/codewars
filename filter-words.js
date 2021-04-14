9eb427eddd776edd3ec2a61ca34e973232135a04
JakesProjects1 <jacob@kagon.net>
2021-04-13 17:30:42 -0700

    refactored to not include .filter

--- filter-words.js
+++ filter-words.js
@@ -4,3 +4,17 @@ function filterLongWords(sentence, n) {
   
    return sentenceArray.filter(word => word.length > n);
   }
+// without using .filter
+  function filterLongWords(sentence, n) {
+    //Solution here
+    
+    let sentenceArray = sentence.split(" ");
+    let newWordArray = [];
+    
+    for(let i = 0; i < sentenceArray.length; i++) {
+      if(sentenceArray[i].length > n) {
+        newWordArray.push(sentenceArray[i]);
+      }
+    }
+    return newWordArray;
+  }
