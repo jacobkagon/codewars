4f5b1936ce68680883a8a4df324f23488d0d84c0
JakesProjects1 <jacob@kagon.net>
2021-04-14 14:44:54 -0700

    done with remove first and last letters

---
+++ remove-first-and-last.js
@@ -0,0 +1,7 @@
+function removeChar(str){
+    //You got this!
+    //use substring and slice to remove the first and last elements of a string;
+     const newStr = str.substring(1).slice(0, -1)
+     
+     return newStr
+   };
